be7eea37b0a3d8474b981e8ff2e9f429e31021e0
wngfra <wngfra@gmail.com>
2020-07-08 11:28:52 +0200

    fixed rclpy parameter passing

--- can_wifi/can_wifi/tactile_signal_publisher.py
+++ can_wifi/can_wifi/tactile_signal_publisher.py
@@ -4,7 +4,7 @@ import sys
 import rclpy
 from rclpy.node import Node
 
-from tactile_msg.msg import TactileSignal
+from tactile_msgs.msg import TactileSignal
 
 
 class TactileSignalPublisher(Node):
@@ -20,20 +20,20 @@ class TactileSignalPublisher(Node):
             ]
         )
         (ip, port) = self.get_parameters(['ip', 'port'])
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((str(ip.value), int(port.value)))
 
-        self.sock = sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind((ip, port))
-
-        self.publisher_ = self.create_publisher(TactileSignal, 'topic', 10)
+        self.publisher_ = self.create_publisher(TactileSignal, '/tactile_signals', 10)
         timer_period = 0.03  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         msg = TactileSignal()
         msg.header.frame_id = 'world'
-        msg.header.stamp = self.get_clock().now()
-        data, addr = sock.recvfrom(1024)
-        msg.addr = addr[0] + ":" + addr[1]
+        msg.header.stamp = self.get_clock().now().to_msg()
+        data, addr = self.sock.recvfrom(1024)
+        msg.addr = addr[0] + ":" + str(addr[1])
         msg.data = values = [int.from_bytes(
             data[i:i+2], 'big', signed=False) for i in range(0, len(data), 2)]
         self.publisher_.publish(msg)
