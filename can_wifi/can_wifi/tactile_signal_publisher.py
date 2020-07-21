78f216de626b83ab88e2e6a31bf7559d44363d93
wngfra <wngfra@gmail.com>
2020-07-21 16:56:02 +0200

    using collections.deque to store calibration samples

--- can_wifi/can_wifi/tactile_signal_publisher.py
+++ can_wifi/can_wifi/tactile_signal_publisher.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2020 wngfra
 # Use of this source code is governed by the Apache-2.0 license, see LICENSE
 
+from collections import deque
 import socket
 import sys
+
 
 import numpy as np
 import rclpy
@@ -29,61 +31,50 @@ class TactileSignalPublisher(Node):
         self.sock_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock_.bind((str(ip.value), int(port.value)))
 
-        self.initialized_ = False
         self.state_ = 0
-        self.calibration_size = 30 # Number of samples used for calibration
-        self.calibration_queue_ = []
+        self.calibration_queue_ = deque(maxlen=30) # Number of samples used for calibration
         self.reference_values_ = np.zeros(16)
 
         self.publisher_ = self.create_publisher(
             TactileSignal, '/tactile_signals', 10)
         self.srv_ = self.create_service(
-            ChangeStage, '/tactile_publisher/change_state', self.change_state_callback)
+            ChangeState, '/tactile_publisher/change_state', self.change_state_callback)
 
         # Publisher rate 0.03s
         self.timer_ = self.create_timer(0.03, self.timer_callback)
 
     def timer_callback(self):
         data, addr = self.sock_.recvfrom(1024)
-        values = [int.from_bytes(
-            data[i:i+2], 'big', signed=False) for i in range(0, len(data), 2)]
+        values = [int.from_bytes(data[i:i+2], 'big', signed=False) for i in range(0, len(data), 2)]
 
-        if self.state_ == 1:
-            if not self.initialized_:
-                self.get_logger().info("Tactile sensor has not been calibrated yet!")
+        if self.state_ == 1 and len(self.calibration_queue_) == self.calibration_queue_.maxlen:
+            self.reference_values_ = np.average(self.calibration_queue_, axis=0)
 
             msg = TactileSignal()
             msg.header.frame_id = 'world'
             msg.header.stamp = self.get_clock().now().to_msg()
             try:
                 msg.addr = addr[0] + ":" + str(addr[1])
-                msg.data = np.array(values, dtype=np.int32) - \
-                    self.reference_values_.astype(np.int32)
+                msg.data = np.array(values, dtype=np.int32) - self.reference_values_.astype(np.int32)
                 self.publisher_.publish(msg)
             except:
                 self.get_logger().error("Error tactile data: incorrect array length or overflow.")
         elif self.state_ == 0:
             if len(values) == 16:
-                if len(self.calibration_queue_) < self.calibration_size:
-                    self.calibration_queue_.append(values)
-                else:
-                    self.initialized_ = True
-                    self.calibration_queue_.pop()
-                    self.calibration_queue_.append(values)
-                    self.reference_values_ = np.average(
-                        self.calibration_queue_, axis=0)
+                self.calibration_queue_.append(values)
+        else:
+            self.get_logger().error("Unknown state or uncalibrated sensor!")
 
     def change_state_callback(self, request, response):
         if request.transition != self.state_:
             try:
                 self.state_ = request.transition
 
-                print(self.state_)
-
                 response.success = True
                 response.info = "OK"
             except:
-                pass
+                response.success = False
+                response.info = "Undefined state!"
         else:
             response.success = True
             response.info = "No transition needed!"
