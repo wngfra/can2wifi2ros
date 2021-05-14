fixing random broadcast errors

can_wifi/can_wifi/tactile_signal_publisher.py
@@ -1,9 +1,7 @@
 # Copyright (c) 2020 wngfra
 # Use of this source code is governed by the Apache-2.0 license, see LICENSE
 
-from collections import deque
 import socket
-import sys
 
 import numpy as np
 import rclpy
@@ -17,12 +15,7 @@ THRESHOLD_MU = 1
 THRESHOLD_SIGMA = 1.0
 
 
-STATE_LIST = {
-    0:  'calibration',
-    1:  'recording',
-    50: 'standby',
-    99: 'termination'
-}
+STATE_LIST = {0: "calibration", 1: "recording", 50: "standby", 99: "termination"}
 
 
 class TactileSignalPublisher(Node):
@@ -33,89 +26,92 @@ class TactileSignalPublisher(Node):
     """
 
     def __init__(self):
-        super().__init__('tactile_publisher')
+        super().__init__("tactile_publisher")
 
         # Parameters are set via ROS2 parameter server.
 
         self.declare_parameters(
-            namespace='',
+            namespace="",
             parameters=[
-                ('ip',               "0.0.0.0"),
-                ('port',             10240),
-                ('calibration_size', 30),
-                ('mode',             None)
-            ]
+                ("ip", "0.0.0.0"),
+                ("port", 10240),
+                ("buffer_size", 64),
+                ("mode", None),
+            ],
         )
-        ip = str(self.get_parameter('ip').value)
-        port = int(self.get_parameter('port').value)
-        calibration_size = int(self.get_parameter('calibration_size').value)
-        mode = str(self.get_parameter('mode').value)
+        ip = str(self.get_parameter("ip").value)
+        port = int(self.get_parameter("port").value)
+        buffer_size = int(self.get_parameter("buffer_size").value)
+        mode = str(self.get_parameter("mode").value)
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((ip, port))
-
+        self.mode = mode
         self.node_state = 0
 
         # Queue of data for calibration
-        self.calibration_queue = deque(maxlen=calibration_size)
+        self.buffer = np.zeros((buffer_size, 16))
         self.reference_value = np.zeros(16)
+        self.buffer_count = 0
 
-        self.publisher = self.create_publisher(
-            TactileSignal, 'tactile_signals', 10)
+        self.publisher = self.create_publisher(TactileSignal, "tactile_signals", 10)
         self.service = self.create_service(
-            ChangeState, 'tactile_publisher/change_state', self.change_node_state_callback)
+            ChangeState,
+            "tactile_publisher/change_state",
+            self.change_node_state_callback,
+        )
 
         # Publisher rate 0.03s
-        callback_func = self.sim_callback if mode == 'sim' else self.timer_callback
-        self.timer = self.create_timer(0.03, callback_func)
+        self.timer = self.create_timer(0.03, self.timer_callback)
 
-        self.get_logger().info('Node started in state: calibration')
+        self.get_logger().info("Node started in state: calibration")
 
-    def sim_callback(self):
-        msg = TactileSignal()
-        msg.header.frame_id = 'world'
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.addr = 'sim'
-        msg.data = np.random.randint(0, 100, size=16, dtype=np.int32)
-        msg.mean = np.mean(msg.data)
-        self.publisher.publish(msg)
+    def process(self, values):
+        processed_ = values - self.reference_value
+        # processed_[8] = 0
+        return processed_
 
     def timer_callback(self):
-        data, addr = self.sock.recvfrom(1024)
-        values = [int.from_bytes(data[i:i+2], 'big', signed=False)
-                  for i in range(0, len(data), 2)]
+        data, addr = self.sock.recvfrom(256)
+        values = np.array(
+            [
+                int.from_bytes(data[i : i + 2], "big")
+                for i in range(0, len(data), 2)
+            ],
+            dtype=np.int32,
+        )
+        self.get_logger().info("{}-byte raw data {}".format(len(data), data))
 
-        if len(values) == 16:
+        try:
             if self.node_state == 0:  # calibration state
-                self.calibration_queue.append(values)
+                self.buffer[:-1] = self.buffer[1:]
+                self.buffer[-1] = values
+                self.buffer_count += 1
+                if self.buffer_count == self.buffer.shape[0]:
+                    self.reference_value = np.mean(self.buffer, axis=0, dtype=np.int32)
+                    self.get_logger().info("Calibration finished!")
             elif self.node_state == 1:  # recording state
-                if len(self.calibration_queue) == self.calibration_queue.maxlen:
-                    self.reference_value = np.mean(
-                        self.calibration_queue, axis=0)
+                if self.buffer_count < self.buffer.shape[0]:
+                    self.get_logger().warn("Calibration unfinished!")
 
-                    msg = TactileSignal()
-                    msg.header.frame_id = 'world'
-                    msg.header.stamp = self.get_clock().now().to_msg()
-                    try:
-                        data = np.array(values, dtype=np.int32) - self.reference_value.astype(np.int32)
-                        data[8] = 0
-                        if np.mean(data) <= THRESHOLD_MU and np.var(data) >= THRESHOLD_SIGMA**2:
-                            data.fill(0)
+                msg = TactileSignal()
+                msg.addr = addr[0] + ":" + str(addr[1])
+                msg.header.frame_id = "world"
+                msg.header.stamp = self.get_clock().now().to_msg()
 
-                        msg.addr = addr[0] + ":" + str(addr[1])
-                        msg.data = data
-                        msg.mean = np.mean(data)
-                        self.publisher.publish(msg)
-                    except Exception as error:
-                        self.get_logger().error(str(error))
-                else:
-                    self.get_logger().error("Uncalibrated sensor!")
-                    raise Exception("Calibration queue is not filled.")
+                if self.mode == "processed":
+                    values = self.process(values)
+
+                msg.data = values
+                msg.mean = np.mean(values)
+                self.publisher.publish(msg)
             elif self.node_state == 50:  # standby state
                 pass
             elif self.node_state == 99:  # termination state
                 self.get_logger().warn("Tactile publisher terminated.")
                 self.destroy_node()
+        except Exception as error:
+            self.get_logger().error(str(error))
 
     def change_node_state_callback(self, request, response):
         if request.transition != self.node_state:
@@ -127,14 +123,17 @@ class TactileSignalPublisher(Node):
                     response.info = "OK"
 
                     self.get_logger().info(
-                        "Changed to state: {}".format(STATE_LIST[self.node_state]))
+                        "Changed to state: {}".format(STATE_LIST[self.node_state])
+                    )
                 else:
                     raise Exception("Undefined state")
             except Exception as error:
                 response.success = False
                 response.info = str(error)
 
-                self.get_logger().error("Wrong transition! Reverting to state: calibration")
+                self.get_logger().error(
+                    "Wrong transition! Reverting to state: calibration"
+                )
                 self.node_state = 0
 
         return response
@@ -147,5 +146,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()